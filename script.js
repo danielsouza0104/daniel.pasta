f705e207a25173f658031794e704470fc44e8acf
danielsouza0104 <danielrsouza789@gmail.com>
2025-04-12 15:31:08 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let botao = document.querySelector("#botao");
-
-botao.addEventListener("click", mensagem);
-
-function mensagem(){
-    botao.innerHTML= "Seja bem-vindo(a) ao meu site!!";
-    botao.style.background= "lightgreen";
-};
